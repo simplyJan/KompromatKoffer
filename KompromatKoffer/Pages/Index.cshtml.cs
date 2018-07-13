bd70d230d2304f056bb23b8dc56d5b0d6ede6a31
Scobiform <uwe@scobiform.com>
2018-07-13 13:58:04 +0200

    Initial Commit

---
+++ KompromatKoffer/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Tweetinvi.Parameters;
+
+namespace KompromatKoffer.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public void OnGet()
+        {
+
+            Auth.SetUserCredentials(Config.Credentials.CONSUMER_KEY,Config.Credentials.CONSUMER_SECRET, Config.Credentials.ACCESS_TOKEN,Config.Credentials.ACCESS_TOKEN_SECRET);
+
+
+
+            
+
+        }
+
+
+
+
+    }
+}
