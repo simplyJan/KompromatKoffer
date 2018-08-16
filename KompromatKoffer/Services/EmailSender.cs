e3dbf790585c4bee066e8692c2c21ec0d7b6a724
Scobiform <uwe@scobiform.com>
2018-08-16 19:34:19 +0200

    emailsender & admin view

--- KompromatKoffer/Services/EmailSender.cs
+++ KompromatKoffer/Services/EmailSender.cs
@@ -3,18 +3,65 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net; 
+using System.Net.Mail;
 
 namespace KompromatKoffer.Services
 {
-    
-    // This class is used by the application to send email for account confirmation and password reset.
-    // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string message)
+        public async Task SendEmailAsync(string To_Email_Address, string subject, string message)
+
         {
-            return Task.CompletedTask;
+            if (To_Email_Address == null) return; 
+
+            try
+            {
+
+                string sFrom_Email_Address = Config.Parameter.Mail_From_Email_Address;
+
+                string sFrom_Email_DisplayName = Config.Parameter.Mail_From_Email_DisplayName;
+
+                string sHost = Config.Parameter.Mail_Host;
+
+                string intPort = Config.Parameter.Mail_Port;
+
+                string sEmail_Login = Config.Parameter.Mail_Email_Login;
+
+                string sEmail_Passwort = Config.Parameter.Mail_Email_Passwort;
+
+
+                MailMessage email = new MailMessage();
+
+                email.To.Add(new MailAddress(To_Email_Address));
+
+                email.From = new MailAddress(sFrom_Email_Address, sFrom_Email_DisplayName);
+
+                email.Subject = subject;
+
+                email.Body = message;
+
+                email.IsBodyHtml = true;
+
+
+                SmtpClient smtp = new SmtpClient();
+
+                smtp.Host = sHost;
+
+                smtp.Port = Convert.ToInt32(intPort);
+
+                smtp.Credentials = new NetworkCredential(sEmail_Login, sEmail_Passwort);
+
+                smtp.EnableSsl = false;
+
+
+                await smtp.SendMailAsync(email);
+            }
+            catch (Exception ex)
+
+            {
+                Console.WriteLine("Error EmailSender.cs error:" + ex.Message);
+            }
         }
     }
-   
 }
