ed26b2f9f27d04f445072fc384cbfe473366b1ea
Scobiform <uwe@scobiform.com>
2018-09-24 12:39:34 +0200

    introducing Database Maintenance Project into Solution

---
+++ DatabaseMaintenance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using KompromatKoffer.Areas.Database.Model;
+using LiteDB;
+
+namespace DatabaseMaintenance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PoliticalPartyUpdate();
+            
+
+        }
+
+
+        private static void PoliticalPartyUpdate()
+        {
+            using (var db = new LiteDatabase("TwitterData.db"))
+            {
+                try
+                {
+                    Console.WriteLine("Getting Collection");
+                    var col = db.GetCollection<TwitterUserModel>("TwitterUser");
+
+
+                    col.Find
+
+
+                }
+                catch(LiteException ex)
+                {
+                    Console.WriteLine("LiteDB Exception: " + ex);
+                }
+
+
+            }
+
+
+
+        }
+
+    }
+}
