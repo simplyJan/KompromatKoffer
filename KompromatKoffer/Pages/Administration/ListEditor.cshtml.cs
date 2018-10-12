1cb4e13cf023f83636bce19fa69362282a0737df
Scobiform <uwe@scobiform.com>
2018-10-12 17:35:39 +0200

    getting lisEditor to work

--- KompromatKoffer/Pages/Administration/ListEditor.cshtml.cs
+++ KompromatKoffer/Pages/Administration/ListEditor.cshtml.cs
@@ -4,15 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using KompromatKoffer.Areas.Database.Model;
 using LiteDB;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace KompromatKoffer.Pages.Administration
 {
+    [Authorize(Roles = "Administrator")]
     public class ListEditorModel : PageModel
     {
-
+       
         private readonly ILogger _logger;
 
         public ListEditorModel(ILogger<ListEditorModel> logger)
@@ -36,13 +38,12 @@ namespace KompromatKoffer.Pages.Administration
 
         public IList<PoliticalPartyModel> PoliticalParty { get; set; }
 
+        public string NewPolPaParty { get; set; }
+
         
 
         public void OnGet()
         {
-
-            
-
             using (var db = new LiteDatabase("TwitterData.db"))
             {
 
@@ -58,17 +59,52 @@ namespace KompromatKoffer.Pages.Administration
                 var completeCollection = col.FindAll();
 
                 CompleteDB = completeCollection;
+                
+            }
+        }
+
+
+        public IActionResult OnPostSaveEntry(long id, string polPa)
+        {
+            using (var db = new LiteDatabase("TwitterData.db"))
+            {
+                _logger.LogInformation(">>>>>>>>> Checking DB for {0} ...", id);
+
+                var col = db.GetCollection<TwitterUserModel>("TwitterUser");
+
+                var changeEntry = col.Find(x => x.Id == id);
+
+                if (changeEntry != null)
+
+                {
+
+                    foreach(var item in changeEntry)
+                    {
+                        _logger.LogInformation(">>>>>>>>> Updated DB Entry\n>>>>>>>>> {0} - {1} - {2}", item.Id, item.Name, item.PoliticalParty);
+                    }
 
 
 
+
+                    //do stuff
+
+                    _logger.LogInformation(">>>>>>>>> Updated DB Entry for {0} ... ", polPa);
+
+                }
 
             }
 
+
+           
+
+            return RedirectToPage();
 
 
 
 
         }
+
+
 
 
 
