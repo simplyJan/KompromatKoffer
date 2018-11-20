f31af78955bb8b3b57e54d6094da9d127cd8aa32
Scobiform <uwe@scobiform.com>
2018-11-20 16:17:19 +0100

    fixing old database null values

--- DatabaseMaintenance/Program.cs
+++ DatabaseMaintenance/Program.cs
@@ -15,9 +15,11 @@ namespace DatabaseMaintenance
             //PoliticalPartyUpdate();
             //FixDBwithLast();
 
+            UpdateTwitterUserDaily();
+
         }
 
- 
+
         private static void PoliticalPartyUpdate()
         {
             using (var db = new LiteDatabase("TwitterData.db"))
@@ -60,10 +62,10 @@ namespace DatabaseMaintenance
                         {
 
 
-                            if (entry.Description.Contains("BÜNDNIS 90/DIE GRÜNEN") 
-                                || entry.Description.Contains("Grüne") 
-                                || entry.Description.Contains("grüne") 
-                                || entry.Description.Contains("Gruene") 
+                            if (entry.Description.Contains("BÜNDNIS 90/DIE GRÜNEN")
+                                || entry.Description.Contains("Grüne")
+                                || entry.Description.Contains("grüne")
+                                || entry.Description.Contains("Gruene")
                                 || entry.Description.Contains("gruene")
                                 )
                             {
@@ -95,9 +97,9 @@ namespace DatabaseMaintenance
                                 col.Update(userModel);
 
                             }
-                            else if (entry.Description.Contains("DIE LINKE") 
-                                || entry.Description.Contains("LINKE") 
-                                || entry.Description.Contains("linke") 
+                            else if (entry.Description.Contains("DIE LINKE")
+                                || entry.Description.Contains("LINKE")
+                                || entry.Description.Contains("linke")
                                 || entry.Description.Contains("linksfraktion")
                                 || entry.Description.Contains("links")
                                 || entry.Description.Contains("DieLinke")
@@ -135,8 +137,8 @@ namespace DatabaseMaintenance
 
 
                             }
-                            else if (entry.Description.Contains("AFD") 
-                                || entry.Description.Contains("AfD") 
+                            else if (entry.Description.Contains("AFD")
+                                || entry.Description.Contains("AfD")
                                 || entry.Description.Contains("afd")
                                 || entry.Screen_name.Contains("afd")
                                 || entry.Screen_name.Contains("AFD")
@@ -171,9 +173,9 @@ namespace DatabaseMaintenance
 
                                 col.Update(userModel);
                             }
-                            else if (entry.Description.Contains("CDU") 
-                                || entry.Description.Contains("CSU") 
-                                || entry.Description.Contains("CDU/CSU") 
+                            else if (entry.Description.Contains("CDU")
+                                || entry.Description.Contains("CSU")
+                                || entry.Description.Contains("CDU/CSU")
                                 || entry.Description.Contains("cducsu")
                                 || entry.Screen_name.Contains("cdu")
                                 || entry.Screen_name.Contains("CDU")
@@ -210,9 +212,9 @@ namespace DatabaseMaintenance
 
                                 col.Update(userModel);
                             }
-                            else if (entry.Description.Contains("FDP") 
-                                || entry.Description.Contains("fdp") 
-                                || entry.Description.Contains("liberal") 
+                            else if (entry.Description.Contains("FDP")
+                                || entry.Description.Contains("fdp")
+                                || entry.Description.Contains("liberal")
                                 || entry.Description.Contains("Freien Demokraten")
                                 || entry.Screen_name.Contains("fdp")
                                 || entry.Screen_name.Contains("FDP")
@@ -246,9 +248,9 @@ namespace DatabaseMaintenance
 
                                 col.Update(userModel);
                             }
-                            else if (entry.Description.Contains("SPD") 
-                                || entry.Description.Contains("spd") 
-                                || entry.Description.Contains("sozialdemokrat") 
+                            else if (entry.Description.Contains("SPD")
+                                || entry.Description.Contains("spd")
+                                || entry.Description.Contains("sozialdemokrat")
                                 || entry.Screen_name.Contains("spd")
                                 || entry.Screen_name.Contains("SPD")
                                 || entry.Name.Contains("SPD")
@@ -281,13 +283,13 @@ namespace DatabaseMaintenance
 
                                 col.Update(userModel);
                             }
-                            else if (entry.Description.Contains("Blaue") 
+                            else if (entry.Description.Contains("Blaue")
                                 || entry.Description.Contains("Blauen")
                                 || entry.Description.Contains("blaue")
                                 || entry.Description.Contains("blau"))
                             {
-                                if(debugLine == true)
-                                Console.WriteLine("Is Blau");
+                                if (debugLine == true)
+                                    Console.WriteLine("Is Blau");
                                 blau.Add(entry.Name);
                                 var userModel = new TwitterUserModel
                                 {
@@ -319,7 +321,7 @@ namespace DatabaseMaintenance
                                     Console.WriteLine("Is NIX");
                                 nix.Add(entry.Name);
                             }
-                                
+
                         }
 
                         //Find equal name  - in list / textfile? - on wikiData? 
@@ -343,7 +345,7 @@ namespace DatabaseMaintenance
 
 
                     Console.WriteLine(">>> Nichts gefunden {0} ", nix.Count());
-                    foreach(var jarnix in nix){Console.WriteLine(jarnix.ToString());}
+                    foreach (var jarnix in nix) { Console.WriteLine(jarnix.ToString()); }
 
                     Console.WriteLine(">>> Abfall {0} ", abfall.Count());
                     foreach (var muell in abfall) { Console.WriteLine(muell.ToString()); }
@@ -408,5 +410,38 @@ namespace DatabaseMaintenance
 
         }
 
+        private static void UpdateTwitterUserDaily()
+        {
+
+            using (var db = new LiteDatabase("TwitterData.db"))
+            {
+                try
+                {
+
+
+                    Console.WriteLine(">>> Getting Collection...");
+                    var col = db.GetCollection<TwitterUserDailyModel>("TwitterUserDaily");
+
+                    var update = col.FindAll().Where(s => s.TwitterName == null);
+
+                    Console.WriteLine(update.Count());
+
+
+
+                }
+                catch (LiteException ex)
+                {
+                    Console.WriteLine("LiteDB Exception: " + ex);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception: " + ex);
+                }
+
+
+                Console.ReadKey();
+
+            }
+        }
     }
 }
