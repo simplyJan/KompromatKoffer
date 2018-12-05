3f736d8f503230247105db0d8152cf663a372b7e
Scobiform <uwe@scobiform.com>
2018-12-05 14:17:26 +0100

    introducing DataScraping

--- KompromatKoffer/Pages/DataScraping/Wahlrecht.cshtml.cs
+++ KompromatKoffer/Pages/DataScraping/Wahlrecht.cshtml.cs
@@ -5,22 +5,50 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Tweetinvi;
+using OpenScraping;
+using OpenScraping.Config;
+using Newtonsoft.Json;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.IO;
 
 namespace KompromatKoffer.Pages.DataScraping
 {
     public class WahlrechtModel : PageModel
     {
+        private readonly ILogger<WahlrechtModel> _logger;
 
+        public WahlrechtModel(ILogger<WahlrechtModel> logger)
+        {
+            _logger = logger;
+        }
 
-        //Scrap Data from Website and build a graph with it
-
-
-
+        //Scrap Data from given websites and build with it
         public void OnGet()
         {
 
+            WebRequest request = WebRequest.Create("http://www.wahlrecht.de/umfragen/index.htm");
+            WebResponse response = request.GetResponse();
+            System.IO.Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
 
-            
+            // www.bbc.com.json contains the JSON configuration file pasted above
+            var configJson = @"
+            {
+                'title': '//h1',
+                'body': '//div[contains(@class, \'article\')]'
+            }
+            ";
+
+            var config = StructuredDataConfig.ParseJsonString(configJson);
+
+            var openScraping = new StructuredDataExtractor(config);
+            var scrapingResults = openScraping.Extract(responseFromServer);
+
+            //Log the Data Scraping
+            _logger.LogInformation(JsonConvert.SerializeObject(scrapingResults, Formatting.Indented));
 
 
 
