c5ea7d5647357740bb3529ff5a09f756d0325f79
Scobiform <uwe@scobiform.com>
2019-03-18 20:01:37 +0100

    Chnaged to complete authorization

--- KompromatKoffer/Pages/Index.cshtml.cs
+++ KompromatKoffer/Pages/Index.cshtml.cs
@@ -9,9 +9,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Tweetinvi.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace KompromatKoffer.Pages
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
 
